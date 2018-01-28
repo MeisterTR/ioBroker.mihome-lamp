1532d752f014d6086cbe78c69bfc36da65e32860
MeisterTR <rensen26@gmail.com>
2018-01-28 06:13:28 +0100

    1.0.0

--- admin/words.js
+++ admin/words.js
@@ -1,10 +1,149 @@
+/*global systemDictionary:true */
+'use strict';
+
 systemDictionary = {
-    "MiHome-Lamp adapter settings": {
-        "de": "MiHome-Lampe Adapter Einstellungen",
-        "ru": "Настройки драйвера MiHome лампы"
+    "title": {
+        "en": "adapter for control of Xiaomi Desk Lamp",
+        "de": "Adapter zur Steuerung der Xiaomi Schreibtischlampe",
+        "ru": "адаптер для управления настольной лампой Xiaomi",
+        "pt": "Adaptador para controle da lâmpada de mesa Xiaomi",
+        "nl": "adapter voor aansturing van Xiaomi Desk Lamp",
+        "fr": "adaptateur pour le contrôle de la lampe de bureau Xiaomi",
+        "it": "adattatore per il controllo della lampada da scrivania Xiaomi",
+        "es": "adaptador para el control de la lámpara de escritorio Xiaomi",
+        "pl": "adapter do sterowania lampą biurkową Xiaomi"
     },
-    "IP address: ":     {"en": "IP address: ",      "de": "IP-Adresse der Lampe: ",     "ru": "IP адрес: "},
-    "Lamp port: ":     {"en": "Lamp port: ",        "de": "Port der Lampe: ",           "ru": "Порт пылесоса: "},
-    "Own port: ":       {"en": "Own port: ",        "de": "Eigener Port: ",             "ru": "Собственный порт: "},
-    "Token: ":          {"en": "Token: ",           "de": "Token: ",                    "ru": "Токен: "}
+    "IP address:": {
+        "en": "IP address:",
+        "de": "IP Adresse:",
+        "ru": "Айпи адрес:",
+        "pt": "Endereço de IP:",
+        "nl": "IP adres:",
+        "fr": "Adresse IP:",
+        "it": "Indirizzo IP:",
+        "es": "Dirección IP:",
+        "pl": "Adres IP:"
+    },
+    "connection settings": {
+        "en": "connection settings",
+        "de": "Verbindungseinstellungen",
+        "ru": "Настройки соединения",
+        "pt": "configurações de conexão",
+        "nl": "verbindingsinstellingen",
+        "fr": "paramètres de connexion",
+        "it": "impostazioni di connessione",
+        "es": "configuración de conexión",
+        "pl": "ustawienia połączenia"
+    },
+    "additional settings": {
+        "en": "additional settings",
+        "de": "zusätzliche Einstellungen",
+        "ru": "дополнительные настройки",
+        "pt": "Configurações adicionais",
+        "nl": "aanvullende instellingen",
+        "fr": "paramètres additionnels",
+        "it": "altre impostazioni",
+        "es": "ajustes adicionales",
+        "pl": "dodatkowe ustawienia"
+    },
+
+    "Invalid token length. Expected 32 or 96 HEX chars.": {
+        "en": "Invalid token length. Expected 32 or 96 HEX chars.",
+        "de": "Ungültige Tokenlänge Erwartete 32 oder 96 HEX-Zeichen.",
+        "ru": "Недопустимая длина токена. Ожидаемые 32 или 96 символов HEX.",
+        "pt": "Comprimento de token inválido. Esperava 32 ou 96 caracteres HEX.",
+        "nl": "Ongeldige tokenlengte. Verwachte 32 of 96 HEX-tekens.",
+        "fr": "Longueur de jeton incorrecte Caractères HEX attendus 32 ou 96.",
+        "it": "Lunghezza del token non valida. Previsti 32 o 96 caratteri HEX.",
+        "es": "Longitud de token inválida. Se esperan 32 o 96 caracteres HEX.",
+        "pl": "Nieprawidłowa długość tokenu. Oczekiwano 32 lub 96 znaków HEX."
+    },
+    "Xiaomi desk Lamp settings": {
+        "en": "Xiaomi desk Lamp settings",
+        "de": "Xiaomi Schreibtischlampe Einstellungen",
+        "ru": "Настольные лампы Xiaomi",
+        "pt": "Configurações da lâmpada da mesa Xiaomi",
+        "nl": "Instellingen voor Xiaomi-bureaulamp",
+        "fr": "Xiaomi desk Paramètres de la lampe",
+        "it": "Impostazioni della lampada da tavolo Xiaomi",
+        "es": "Configuración de la lámpara de escritorio Xiaomi",
+        "pl": "Ustawienia lampy biurkowej Xiaomi"
+    },
+    "Own port:": {
+        "en": "Own port:",
+        "de": "Eigener Port:",
+        "ru": "Собственный порт:",
+        "pt": "Porta própria:",
+        "nl": "Eigen poort:",
+        "fr": "Propre port:",
+        "it": "Porto proprio:",
+        "es": "Puerto propio:",
+        "pl": "Własny port:"
+    },
+    "Request Intervall": {
+        "en": "Request Intervall in ms ",
+        "de": "Abfrage Intervall in ms ",
+        "ru": "Запросить интервал в мс",
+        "pt": "Solicitar Intervall em ms",
+        "nl": "Interval aanvragen in ms",
+        "fr": "Demander Intervall en ms",
+        "it": "Richiesta Intervall in ms",
+        "es": "Solicitar Intervall en ms",
+        "pl": "Popro? Intervall w ms"
+    },
+    "Send own commands": {
+        "en": "Send own commands",
+        "de": "Sende eigene Befehle",
+        "ru": "Отправить собственные команды",
+        "pt": "Mandar comandos próprios",
+        "nl": "Stuur eigen commando's",
+        "fr": "Envoyer ses propres commandes",
+        "it": "Invia i tuoi comandi",
+        "es": "Enviar los propios comandos",
+        "pl": "Wyślij własne polecenia"
+    },
+    "Token": {
+        "en": "token",
+        "de": "Token",
+        "ru": "Знак",
+        "pt": "Símbolo",
+        "nl": "token",
+        "fr": "Jeton",
+        "it": "Gettone",
+        "es": "Simbólico",
+        "pl": "Znak"
+    },
+    "Lamp port:": {
+        "en": "Port of the lamp",
+        "de": "Port der Lampe",
+        "ru": "Порт лампы",
+        "pt": "Porto da lâmpada",
+        "nl": "Poort van de lamp",
+        "fr": "Port de la lampe",
+        "it": "Porto della lampada",
+        "es": "Puerto de la lámpara",
+        "pl": "Port lampy"
+    },
+    "settings": {
+        "en": "settings",
+        "de": "Einstellungen",
+        "ru": "настройки",
+        "pt": "configurações",
+        "nl": "instellingen",
+        "fr": "paramètres",
+        "it": "impostazioni",
+        "es": "ajustes",
+        "pl": "ustawienia"
+    },
+    "add a state for Alexa": {
+        "en": "add a state for Alexa",
+        "de": "Füge einen Status für Alexa hinzu",
+        "ru": "добавить информацию о пользователе Alexa",
+        "pt": "adicione um estado para Alexa",
+        "nl": "voeg een staat toe voor Alexa",
+        "fr": "ajouter un état pour Alexa",
+        "it": "aggiungi uno stato per Alexa",
+        "es": "agregar un estado para Alexa",
+        "pl": "dodaj stan dla Alexy"
+    }
 };
